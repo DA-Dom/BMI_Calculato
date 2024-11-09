30e806a10e51581e11f574e09841133d33be71fd
DA-Dom <148399346+DA-Dom@users.noreply.github.com>
2024-11-09 19:46:35 +0000

    Add files via upload

---
+++ BMI calculator.py
@@ -0,0 +1,12 @@
+height = input("What is your height?(meters): ")
+weight = input("What is your weight?(KG): ")
+heightcal = float(height)
+weightcal = float(weight)
+height1 = heightcal * heightcal
+weight1 = weightcal / height1
+Bmi = print(" Your BMI is" , weight1)
+
+input("Press any key to exit")
+
+
+        
